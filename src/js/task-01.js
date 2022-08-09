6e63992394d8b8c5f3620b544248a3ac4a3a1821
Tkachenko Dmytro <dmytro.tkachenko91@gmail.com>
2022-08-09 20:36:07 +0300

    done first
    done first task

---
+++ src/js/task-01.js
@@ -0,0 +1,74 @@
+/*
+Number of categories: 3
+
+Category: Animals
+Elements: 4
+
+Category: Products
+Elements: 3
+
+Category: Technologies
+Elements: 5
+*/
+// 1. ul количество детей
+// 2. ввыстести имя h2
+// 3. количство li
+const ulElement = document.querySelector('#categories');
+console.log("Number of categories:", ulElement.children.length);
+
+for (const itemLi of ulElement.children) {
+  console.log('\nCategory:', itemLi.children[0].textContent)
+  console.log('Elements:', itemLi.children[1].children.length)
+}
+
+
+
+//////////////////////////////////////////
+
+// const ulElement = document.querySelector('#categories');
+
+// console.log(`Number of categories: ${ulElement.children.length}`);
+
+// console.log([...ulElement.children]);
+
+// [...ulElement.children].forEach(li => {
+//   const categoryItemSubList = li.querySelector('ul');
+
+//   console.log(`\nCategory: ${li.firstElementChild.textContent}`);
+//   console.log(`Elements: ${categoryItemSubList.children.length}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
