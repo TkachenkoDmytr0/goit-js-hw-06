4e6524d0d0401d31ac2ab230d8d12c5c10d3b6f0
Tkachenko Dmytro <dmytro.tkachenko91@gmail.com>
2022-08-20 18:23:20 +0300

    task four
    done task four

---
+++ src/js/task-04.js
@@ -0,0 +1,38 @@
+// Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
+// Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
+// Обновляй интерфейс новым значением переменной counterValue.
+
+
+let counterValue = 0;
+
+const targetBtnPlus = document.querySelector('button[data-action="increment"]');
+const targetBtnMinus = document.querySelector('button[data-action="decrement"]');
+const addEventListenerNumber = document.querySelector('#value')
+
+
+targetBtnPlus.addEventListener("click", () => {
+    counterValue++
+    addEventListenerNumber.textContent = counterValue
+    
+    console.log("+1");
+});
+targetBtnMinus.addEventListener("click", () => {
+    counterValue--
+    addEventListenerNumber.textContent = counterValue
+    
+    console.log("-1");
+});
+
+
+
+
+// buttonPlus.addEventListener("click", () => {
+//   console.log("Button was clicked");
+// });
+
+
+
+
+
+
+
